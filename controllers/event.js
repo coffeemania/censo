dd9de90b45370d11acd52d7f0c4b20e06117ed93
midik <midiman@ya.ru>
2018-12-15 01:32:50 +0300

    check status #1

--- controllers/event.js
+++ controllers/event.js
@@ -1,6 +1,7 @@
 import op from 'object-path';
-import {Event, Vehicle} from '../models';
 import moment from 'moment';
+import {Event} from '../models';
+import {Provider} from '../lib/Provider';
 
 
 /**
@@ -22,4 +23,27 @@ export const get = async (ctx) => {
         datetime: moment(event.datetime).format('DD.MM.YYYY HH:mm'),
         statusCheckUrl: process.env.STATUS_CHECK_URL ? `${process.env.STATUS_CHECK_URL}${event.foreignId}` : event.foreignId
     });
+};
+
+/**
+ * Get a status for the event
+ * @param ctx
+ */
+export const getAppealStatus = async (ctx) => {
+    const id = op.get(ctx, 'params.id');
+
+    if (!id) throw new Error('Got empty id');
+
+    const events = await Event.query()
+        .alias('e')
+        .eager('vehicle')
+        .where('e.id', '=', id);
+
+    const eventForeignId = op.get(events.shift(), 'foreignId');
+
+    if (!eventForeignId) throw new Error('Got no event');
+
+    const result = await Provider.getAppealStatus(eventForeignId);
+
+    ctx.ok(result);
 };
--- routes/event.js
+++ routes/event.js
@@ -9,6 +9,7 @@ const router = new Router();
  * CRUD for event
  */
 router.get('/:id', event.get);
+router.get('/:id/status', event.getAppealStatus);
 // router.post('/', event.post);
 // router.put('/:id', event.put);
 // router.del('/:id', event.del);
