6d58d48ea3748fee84e061b025e211ff8979f05c
midik <midiman@ya.ru>
2018-12-16 21:59:35 +0300

    reset to first page on filter change

--- ui/src/reducers/eventsPagination.js
+++ ui/src/reducers/eventsPagination.js
@@ -2,6 +2,13 @@ export default (state = '', action = {}) => {
 
     switch (action.type) {
 
+        case 'EVENTS_FILTER':
+
+            return {
+                ...state,
+                page: 1
+            };
+
         case 'GET_EVENTS_SUCCESS':
 
             return {
